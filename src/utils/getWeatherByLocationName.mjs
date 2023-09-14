0efbc7dbd0d24880e77e1760eed0f482ca9029d0
Greg K <113560358+Grtop@users.noreply.github.com>
2023-09-14 12:55:06 +0000

    Update getWeatherByLocationName.mjs

--- src/utils/getWeatherByLocationName.mjs
+++ src/utils/getWeatherByLocationName.mjs
@@ -4,7 +4,7 @@ const API_KEY = "Q7pZjLcobz9aTSJKHfFD07vg5nDO8yDe";
 
 export async function getWeatherByLocationName (loicationName) {
 //const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${loicationName}&appid=${API_KEY}`;
-const url = `https://api.metar-taf.com/metar?api_key=${API_KEY}&v=2.3&locale=en-US&id=LEMD`;
+const url = `https://api.metar-taf.com/metar?api_key=${API_KEY}&v=2.3&locale=en-US&id=${loicationName}`;
 
 const response = await fetch(url);
 const data = response.json();
